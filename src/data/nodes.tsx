c7076e912c229c4b8ee26fc078744fa72db8942c
[dyad] <guiroesler2@gmail.com>
2025-10-21 18:04:46 -0300

    [dyad] Melhorados os estilos visuais do mapa mental - wrote 2 file(s)

--- src/data/nodes.tsx
+++ src/data/nodes.tsx
@@ -6,7 +6,8 @@ export const initialNodes = [
         position: { x: 0, y: 0 },
         deletable: false,
         style: {
-            border: '1px solid #7a28ff',
+            border: '2px solid #6F34DC',
+            borderRadius: '8px',
         }
     },
 ];
